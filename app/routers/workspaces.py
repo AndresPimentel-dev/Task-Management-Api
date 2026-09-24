402c2e450e4b9cff7451f1170fe11eaf70941634
Andres Pimentel <pimentelandressantiago@gmail.com>
2026-09-23 19:25:38 -0500

    final

--- app/routers/workspaces.py
+++ app/routers/workspaces.py
@@ -13,7 +13,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1.0/auth/login")
 
 
 def get_current_user(
-    token: str = Depends(oauth2_scheme),  # noqa: B008
+    token: str = Depends(oauth2_scheme),
     db: Session = Depends(get_db),  # noqa: B008
 ):
     credentials_exception = HTTPException(
